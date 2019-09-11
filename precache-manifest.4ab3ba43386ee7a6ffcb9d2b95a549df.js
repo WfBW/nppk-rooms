e73a1febd52a1957e371fad8b679d37adf09975b
Petr Efimov (Shkoda) <apple-i-shop@yandex.ru>
2019-09-11 15:13:30 +0300

    First push

---
+++ precache-manifest.4ab3ba43386ee7a6ffcb9d2b95a549df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1c1de55a7efceb5184ccf5e5d77485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84078a342b4584f7322f",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4fb6b18baed96f3c5d4e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "84078a342b4584f7322f",
+    "url": "/static/js/2.60e9039d.chunk.js"
+  },
+  {
+    "revision": "4fb6b18baed96f3c5d4e",
+    "url": "/static/js/main.e1976b0f.chunk.js"
+  },
+  {
+    "revision": "8fe988784f2457be2f3c",
+    "url": "/static/js/runtime~main.e4d1af58.js"
+  }
+]);
